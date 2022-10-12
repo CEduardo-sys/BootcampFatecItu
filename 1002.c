b6f92a318360f71d7db14310fc11cf16a9615360
CEduardo-sys <c-eduardo94@hotmail.com>
2022-10-12 11:11:04 -0300

    ex1002 - Area do circulo

---
+++ 1002.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*1002 Area do Circulo*/
+
+int main() {
+ 
+    double raio,area;
+    scanf("%lf",&raio);
+    area=3.14159*raio*raio;
+    printf("A=%.4lf\n",area);
+
+    return 0;
+}
